f3e7f022a286968efcb183b3eb6b47c66b7f434e
yash-kumar-nayak <yashkumarnayak15357@gmail.com>
2023-07-09 12:41:09 +0530

    first version

---
+++ javaapplication14/JavaApplication14.java
@@ -0,0 +1,12 @@
+
+package javaapplication14;
+
+
+public class JavaApplication14 {
+
+  
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ javaapplication14/Main.java
@@ -0,0 +1,11 @@
+
+package javaapplication14;
+
+
+public class Main {
+    public static void main(String[]  args){
+        mainmenu mm=new mainmenu();
+        mm.setVisible(true);
+        mm.setLocationRelativeTo(null);
+    }
+}
